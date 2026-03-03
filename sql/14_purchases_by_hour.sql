c8dc41f78771687fdc6621a3f25a767e43c2c9cb
marrtynkovallchuk <marrtyn.kovallchuk@gmail.com>
2026-03-03 15:34:26 +0100

    Update 14_purchases_by_hour.sql

--- sql/14_purchases_by_hour.sql
+++ sql/14_purchases_by_hour.sql
@@ -1,1 +1,11 @@
+-- Purchases by hour
+-- Returns number of purchases and revenue per hour of the day
 
+SELECT
+    EXTRACT(HOUR FROM event_time) AS hour,
+    COUNT(*) AS purchases,
+    SUM(price) AS revenue
+FROM `ecommerce-project-488216.ecommerce_project.ecommerce_project`
+WHERE event_type = 'purchase'
+GROUP BY hour
+ORDER BY hour;
